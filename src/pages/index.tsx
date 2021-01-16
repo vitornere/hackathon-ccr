592546ff47a095c44ab4686b6c346c5bf9bd1317
Vitor Nere <vitor.nere@hotmail.com>
2021-01-16 18:27:05 -0300

    Configuring amplify

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 import Head from 'next/head';
-import { Container } from './styles';
+import { Container } from '../styles';
 
 export default function Home() {
   return (
     <Container>
       <Head>
-        <title>Create Next App</title>
+        <title>Mentorando</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
